58f6f3b62f6e0e416310e9cd58238f8cb45a0ff1
JoshuaNajera <joshua.najera@g.austincc.edu>
2021-09-17 22:18:16 -0500

    hw-02

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,4 +1,12 @@
-﻿//example
+﻿#ifndef HWEXPRESSIONS_H
+#define HWEXPRESSIONS_H
+
+    double get_sales_tax_amount(double meal_amount);
+    double get_tip_amount(double meal_amount,double tip_rate);
+
+#endif
+
+//example
 int add_numbers(int num1, int num2);
 
 //write function prototype here
