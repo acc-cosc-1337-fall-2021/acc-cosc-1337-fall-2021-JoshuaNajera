dafc97964492117978adee06e31b72d7d4dfecda
JoshuaNajera <joshua.najera@g.austincc.edu>
2021-09-24 22:37:01 -0500

    Hw-03-decisions

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,38 @@
-//write include statement for decisions header
-
-
-//Write code for function(s) code here
+char get_letter_grade_using_if(int grade)
+{
+    if(grade>=90 && grade<=100)
+        return 'A';
+    else if(grade>=80)
+        return 'B';
+    else if(grade>=70)
+        return 'C';
+    else if(grade>=60)
+        return 'D';
+    else
+        return 'F';
+}
+char get_letter_grade_using_switch(int grade)
+{
+    int x=grade/10;
+    char result;
+    switch(x)
+    {
+        case 10:
+            result='A';
+            break;
+        case 9:
+            result='A';
+            break;
+        case 8:
+            result='B';
+            break;
+        case 7:
+            result='C';
+            break;
+        case 6:
+            result='D';
+            break;
+        default:
+            result='F';
+            break;
+    }
