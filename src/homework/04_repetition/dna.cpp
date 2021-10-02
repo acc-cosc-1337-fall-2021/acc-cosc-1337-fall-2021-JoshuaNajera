6327eb0fb5ee415ee2e4a6703c8479f497327797
JoshuaNajera <joshua.najera@g.austincc.edu>
2021-10-01 22:41:11 -0500

    hw-04-repetition

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,15 @@
-//add include statements
 
-//add function(s) code here
+//find factorial
+
+int factorial(int num) {
+  
+   
+   int fact = 1; //intially factorial 
+  
+   for (int i=1; i<=num; i++) { //from 1 to num 
+      
+       fact =fact*i; //find factorial
+   }
+
+   return fact; //return factorial of a number 
+}
--- src/homework/04_repetition/dna.h
+++ src/homework/04_repetition/dna.h
@@ -1,5 +1,26 @@
-//add include statements
+// User-defined function to find gcd of two numbers
+int gcd(int a, int b);
+int main()
+{
+  int num1, num2;
 
-/*
-Write prototype for function(s)
-*/
+  printf("Enter two numbers : ");
+  scanf("%d %d",&num1, &num2);
+
+  int result = gcd(num1, num2);
+  printf("The GCD of %d and %d = %d", num1, num2, result);
+
+  return 0;
+}
+int gcd(int a, int b)
+{
+  int hcf;
+  for(int i=1; i<=a && i<=b; i++)
+  {
+    if(a%i==0 && b%i==0)
+    {
+      hcf = i;
+    }
+  }
+  return hcf;
+}
